58acd9558cddbf205cea63233eaa8a0f451a612b
Meet-Vora <meetrajivvora@gmail.com>
2021-04-11 13:14:41 -0700

    created database.py

---
+++ src/main/database/database.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+class Database:
+
+    def __init__(self):
+        database_name = "database.db"
+        conn = sqlite3.connect(database_name)
+
+    
+    # create_table() method with passed-in schema
+    # read_table() method
+    # close()
+
+
+    # read 
