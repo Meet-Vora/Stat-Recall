218a94df82a85b973ca47c25351c99a9c1ae5191
Meet-Vora <meetrajivvora@gmail.com>
2021-04-29 18:49:50 -0700

    minor changes

--- src/database/setup_database.py
+++ src/database/setup_database.py
@@ -5,15 +5,32 @@ db = Database()
 # Create tables
 db.create_champion_metadata_table()
 db.create_champion_stats_table()
+print("HELLO")
 
-# Write all info to tables
+# Write all info to tables -- WORKS PERFECTLY!
 # db.write_all_champions_metadata()
-# db.write_all_champions_stats()
+# print("DONE WRITING 1")
+db.write_all_champions_stats()
+print("DONE WRITING 2")
 
 # Read all info in tables
-# data = db.get_all_champion_metadata()
-data = db.get_champion_metadata("Aatrox")
-# print(data)
+metadata = db.get_champion_metadata("Aatrox")
+statdata = db.get_champion_stats("Aatrox")
+print("DONE READING")
+
+
+def pretty(d, indent=0):
+    for key, value in d.items():
+        print('\t' * indent + str(key))
+        if isinstance(value, dict):
+            pretty(value, indent+1)
+        else:
+            print('\t' * (indent+1) + str(value))
+
+
+pretty(metadata)
+print("====================================")
+pretty(statdata)
 
 
 # Close db instance
